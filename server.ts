ecb3424d70eaf2a2225ef94ea8d967f0bb78c1d8
Amos Omondi <16223682+amos-o@users.noreply.github.com>
2021-08-20 09:57:49 +0300

    Add files

---
+++ server.ts
@@ -0,0 +1,22 @@
+import { Application, Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+import questions from "./questions.ts";
+
+const app = new Application();
+const port = 8000;
+const router = new Router();
+
+router.get("/", (context) => {
+  context.response.type = "application/json";
+  context.response.body = { questions };
+});
+
+app.addEventListener("error", (event) => {
+  console.error(event.error);
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen({ port });
+
+console.log(`Server is running on port ${port}`);
+
+export default app;
